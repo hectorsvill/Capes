ca01446f7afc3fa385a9702f0386a8b6d35c9b49
hectorsvill <hectorstevenvillasano@gmail.com>
2019-07-29 13:05:43 -0700

    capes init

---
+++ Capes/Capes/CapesViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CapesViewController.swift
+//  Capes
+//
+//  Created by Hector Steven on 7/29/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import UIKit
+
+class CapesViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
