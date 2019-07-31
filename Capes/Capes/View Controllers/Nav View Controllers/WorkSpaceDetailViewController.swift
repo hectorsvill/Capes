bcd8eda7ec3fe19e7dc88f1b4e664163b288b609
hectorsvill <hectorstevenvillasano@gmail.com>
2019-07-31 15:35:10 -0700

    detail view setup - not map view

--- Capes/Capes/View Controllers/Nav View Controllers/WorkSpaceDetailViewController.swift
+++ Capes/Capes/View Controllers/Nav View Controllers/WorkSpaceDetailViewController.swift
@@ -22,7 +22,9 @@ class WorkSpaceDetailViewController: UIViewController {
 	@IBOutlet var cityLabel: UILabel!
 	@IBOutlet var hostBioLabel: UILabel!
 	@IBOutlet var bioTextField: UITextView!
-	@IBOutlet var adressLabel: UILabel!
+	
+	@IBOutlet var addressTextField: UITextView!
+	
 	@IBOutlet var mapView: MKMapView!
 	@IBOutlet var adreessLabel: UILabel!
 	
@@ -51,7 +53,7 @@ class WorkSpaceDetailViewController: UIViewController {
 		cityLabel.text = workspace.city
 		hostBioLabel.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 		bioTextField.text = workspace.bio
-		adressLabel.text = workspace.address + "\n \(workspace.city), CA \(workspace.zipcode)"
+		addressTextField.text = workspace.address + "\n \(workspace.city), CA \(workspace.zipcode)"
 		
 		let url = URL(string: workspace.imageUrl)!
 		URLSession.shared.dataTask(with: url) { (data, _, error) in
