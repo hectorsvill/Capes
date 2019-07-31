be26c882b9cc31f44180d393ed463be52f0b65a3
hectorsvill <hectorstevenvillasano@gmail.com>
2019-07-30 22:40:00 -0700

    implemented controller

--- Capes/Capes/Model Controller/CapesController.swift
+++ Capes/Capes/Model Controller/CapesController.swift
@@ -10,39 +10,67 @@ import UIKit
 
 class CapeController {
 	var allWorkSpaceListing: [String: WorkSpace] = [:]
-	var cityWorkSpaceListting: [WorkSpace] = []
-	
+
 	init() {
 //		createTestData()
-		fetchAllWorkSpaces(in: FireStoreReferenceManager.northHollywoodCa)
+//		fetchAllWorkSpaces(in: FireStoreReferenceManager.northHollywoodCa)
 	}
 	
 	
 	
-	func fetchAllWorkSpaces(in city: String) {
+	func fetchAllWorkSpaces(in city: String, completion: @escaping ([WorkSpace]?, Error?) -> ()) {
 		let fr = FireStoreReferenceManager.root
 		
 		fr.collection(city).getDocuments { snapShot, error in
 			if let error = error {
 				NSLog("Error fetching all workspaces: \(error)")
+				completion(nil, error)
 				return
 			}
-			print("Count of snapshot: \(snapShot!.count)")
-			print(snapShot?.documents[0].data() as! [String: Any])
-//			for document in snapShot!.documents {
-//
-//			}
 			
+			guard let listings = snapShot?.documents else { return }
+			var  workspaces: [WorkSpace] = []
 			
+			for i in 0..<listings.count {
+				let dictionary = (listings[i].data() as [String: Any])
+				let ws = self.createCityWorkSpaceListing(with: dictionary)
+				workspaces.append(ws)
+			}
+
+			completion(workspaces, nil)
 		}
 	}
+	
+	
 	
 	
 
 }
 
-// MARK: Helpers
+// MARK: Helper Methods
 extension CapeController {
+	
+	
+	
+	private func createCityWorkSpaceListing(with dictionary: [String: Any]) -> WorkSpace{
+//		print(dictionary.keys)
+		let name = dictionary["name"] as! String
+		let city = dictionary["city"] as! String
+		let address = dictionary["address"] as! String
+		let zipcode = dictionary["zipcode"] as! String
+		
+		let title = dictionary["title"] as! String
+		let bio = dictionary["bio"] as! String
+		let imageUrl = dictionary["imageUrl"] as! String
+		let price = dictionary["Price"] as! String
+		
+		let perk1 = dictionary["perk1"] as! String
+		let perk2 = dictionary["perk2"] as! String
+		let perk3 = dictionary["perk3"] as! String
+		
+		return WorkSpace(name: name, city: city, address: address, zipcode: zipcode, title: title, bio: bio, imageUrl: imageUrl, price: price, perk1: perk1, perk2: perk2, perk3: perk3)
+	}
+	
 	private func createTestData() {
 		let superWorkSpaceData: [String: Any] = [
 			"name": "Super Work Space",
--- Capes/Capes/View Controllers/WorkSpacesTableViewController.swift
+++ Capes/Capes/View Controllers/WorkSpacesTableViewController.swift
@@ -13,26 +13,51 @@ extension WorkSpacesTableViewController: CapeControllerProtocol {
 
 class WorkSpacesTableViewController: UITableViewController {
 	var capeController: CapeController?
+	var workspaces: [WorkSpace] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
 		
     }
 	
+	
 	override func viewWillAppear(_ animated: Bool) {
 		super.viewWillAppear(animated)
 		
+		let city = FireStoreReferenceManager.northHollywoodCa
+		fetchWorkSpaces(with: city)
 		
 	}
 	
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
-    }
-
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return workspaces.count
+	}
+	
 	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let cell = tableView.dequeueReusableCell(withIdentifier: "WorkSpaceCell", for: indexPath)
 		return cell
 	}
+	
+	
+	
+	private func fetchWorkSpaces(with city: String) {
+		capeController?.fetchAllWorkSpaces(in: city) { workspaces, error in
+			if let error = error {
+				NSLog("Error fetching worskspace: \(error)")
+			} else {
+				guard let workspaces = workspaces else { return }
+				self.workspaces = workspaces
+				DispatchQueue.main.async {
+					self.tableView.reloadData()
+				}
+			}
+		}
+		
+	}
 
+}
+
+
+extension WorkSpacesTableViewController {
+	
 }
