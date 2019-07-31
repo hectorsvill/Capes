89f658617f8da4e6283c23984476c3b0dee528e3
hectorsvill <hectorstevenvillasano@gmail.com>
2019-07-31 14:02:34 -0700

    scroll view

---
+++ Capes/Capes/View Controllers/Nav View Controllers/WorkSpaceDetailViewController.swift
@@ -0,0 +1,24 @@
+//
+//  WorkSpaceDetailViewController.swift
+//  Capes
+//
+//  Created by Hector Steven on 7/31/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import UIKit
+
+class WorkSpaceDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+		
+    }
+	
+	@IBAction func backButtonPressed(_ sender: Any) {
+		dismiss(animated: true, completion: nil)
+	}
+	
+	
+	
+}
