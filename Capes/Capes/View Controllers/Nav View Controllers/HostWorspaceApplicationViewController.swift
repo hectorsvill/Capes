5db8f49b173176398e7e745c7bd16cfc09f1676c
hectorsvill <hectorstevenvillasano@gmail.com>
2019-08-01 12:57:23 -0700

    workspace application view

---
+++ Capes/Capes/View Controllers/Nav View Controllers/HostWorspaceApplicationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HostWorspaceApplicationViewController.swift
+//  Capes
+//
+//  Created by Hector Steven on 8/1/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import UIKit
+
+class HostWorspaceApplicationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
