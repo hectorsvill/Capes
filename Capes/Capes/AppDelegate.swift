e4a1a8dbec3a857fbe88aabb6262e2b54c1e53f2
hectorsvill <hectorstevenvillasano@gmail.com>
2019-08-05 23:05:27 -0700

    gidsignin

--- Capes/Capes/AppDelegate.swift
+++ Capes/Capes/AppDelegate.swift
@@ -13,10 +13,23 @@ import GoogleSignIn
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
-	func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+	func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error?) {
+		// ...
+		if let error = error {
+			// ...
+			return
+		}
 		
+		guard let authentication = user.authentication else { return }
+		let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken,
+													   accessToken: authentication.accessToken)
+		// ...
 	}
 	
+	func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!, withError error: Error!) {
+		// Perform any operations when the user disconnects from app here.
+		// ...
+	}
 
 	var window: UIWindow?
 
