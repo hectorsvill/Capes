27477e7fc947409a3087f7b94e29dde12698d984
hectorsvill <hectorstevenvillasano@gmail.com>
2019-08-05 23:03:51 -0700

    gidsignin

--- Capes/Capes/AppDelegate.swift
+++ Capes/Capes/AppDelegate.swift
@@ -8,15 +8,24 @@
 
 import UIKit
 import Firebase
+import GoogleSignIn
+
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
+	func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+		
+	}
+	
 
 	var window: UIWindow?
 
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 		FirebaseApp.configure()
+		
+		GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
+		GIDSignIn.sharedInstance().delegate = self
 		return true
 	}
 
