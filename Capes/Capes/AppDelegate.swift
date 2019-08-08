8c8d4fc445f6dc9bd32751aa2343284692973c0f
hectorsvill <hectorstevenvillasano@gmail.com>
2019-08-08 15:48:15 -0700

    appdelegate

--- Capes/Capes/AppDelegate.swift
+++ Capes/Capes/AppDelegate.swift
@@ -9,19 +9,24 @@
 import UIKit
 import Firebase
 import GoogleSignIn
-
+import FBSDKCoreKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	var window: UIWindow?
 
-
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 		FirebaseApp.configure()
-		
+		// google sign in
 		GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
 		GIDSignIn.sharedInstance().delegate = self
+		
+		//fb sign in
+	
+		
+		
+		
 		return true
 	}
 
